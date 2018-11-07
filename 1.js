59a4ee0add0793a73577aa289f8ccc892a58a3f2
Bao Thanh <drthanhstvv@gmail.com>
2018-11-08 01:44:31 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -12,17 +12,21 @@ $(document).ready(function() {
 	$('.home-m').click(function(event) {
 		/* Act on the event */
 		$('html, body').animate({scrollTop:0},1000);
+		$('.menuleft').removeClass('menuleft-show');
 	});
 	$('.about-m').click(function(event) {
 		/* Act on the event */
 		$('html, body').animate({scrollTop: $('.about').offset().top},1000);
+		$('.menuleft').removeClass('menuleft-show');
 	});
 	$('.services-m').click(function(event) {
 		/* Act on the event */
 		$('html, body').animate({scrollTop: $('.services').offset().top},1000);
+		$('.menuleft').removeClass('menuleft-show');
 	});
 	$('.portfolio-m').click(function(event) {
 		/* Act on the event */
 		$('html, body').animate({scrollTop: $('.portfolio2').offset().top},1000);
+		$('.menuleft').removeClass('menuleft-show');
 	});
 });
