1bbaba858a1587066435b6e867c3aba11d2eb318
Bao Thanh <drthanhstvv@gmail.com>
2018-11-08 01:39:25 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$('.btn-top').click(function(event) {
+		/* go to top */
+		event.preventDefault();
+		$('html, body').animate({scrollTop:0},1000);
+	});
+	$('.btn-menu').click(function(event) {
+		/* button menu click */
+		$('.menuleft').toggleClass('menuleft-show');
+		$('.btn-menu i').toggleClass('fa-bars fa-times');
+	});
+	$('.home-m').click(function(event) {
+		/* Act on the event */
+		$('html, body').animate({scrollTop:0},1000);
+	});
+	$('.about-m').click(function(event) {
+		/* Act on the event */
+		$('html, body').animate({scrollTop: $('.about').offset().top},1000);
+	});
+	$('.services-m').click(function(event) {
+		/* Act on the event */
+		$('html, body').animate({scrollTop: $('.services').offset().top},1000);
+	});
+	$('.portfolio-m').click(function(event) {
+		/* Act on the event */
+		$('html, body').animate({scrollTop: $('.portfolio2').offset().top},1000);
+	});
+});
